60a014c0960ecbb363004f106b165245aa8336ab
Basti Destruction <sebastian@hiebl.cc>
2020-10-23 21:17:55 +0200

    change curl quiet option

--- matrix-build.sh
+++ matrix-build.sh
@@ -4,17 +4,17 @@ set -euo pipefail
 IMAGE="library/nginx"
 
 function getToken() {
-    curl --no-progress-meter --user "$DOCKER_USERNAME:$DOCKER_PASSWORD" "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${IMAGE}:pull" | jq -r '.token'
+    curl -s --user "$DOCKER_USERNAME:$DOCKER_PASSWORD" "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${IMAGE}:pull" | jq -r '.token'
 }
 
 # query all existing image tags
-TAGS=$(curl --no-progress-meter "https://registry.hub.docker.com/v1/repositories/$IMAGE/tags" | jq -r '.[].name')
+TAGS=$(curl -s "https://registry.hub.docker.com/v1/repositories/$IMAGE/tags" | jq -r '.[].name')
 TOKEN=$(getToken)
 
 NEW_TAGS=()
 
 for tag in $TAGS ; do
-    timestamp=$(curl --no-progress-meter "https://registry.hub.docker.com/v2/$IMAGE/manifests/$tag" -H "Authorization:Bearer $TOKEN" | jq -r '.history[0].v1Compatibility' | jq -r '.created')
+    timestamp=$(curl -s "https://registry.hub.docker.com/v2/$IMAGE/manifests/$tag" -H "Authorization:Bearer $TOKEN" | jq -r '.history[0].v1Compatibility' | jq -r '.created')
     ts_seconds=$(date -u -d "$timestamp" +"%s")
     now_seconds=$(date -u +"%s")
     diff=$((now_seconds - ts_seconds))
